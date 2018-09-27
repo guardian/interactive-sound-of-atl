1ccceaf162a7fdb71aa56749af577e1f5d527056
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-09-27 14:55:36 -0400

    sort data

--- scripts/helpers/data.js
+++ scripts/helpers/data.js
@@ -24,25 +24,67 @@ function fetchData(callback) {
     });
 }
 
-function sortResults(data) {
+function setFurniture() {
+    var furniture = {}
+
+    for (var i = 0; i < data.furniture.length; i++) {
+        data[data.furniture[i].option] = data.furniture[i].value
+    }
+
+    delete data.furniture;
+
+    return data;
+}
+
+function sortResults() {
     if (data.length === 0) {
         data = data[0]
     } else {
         data = {
-            'sheet1': data[0],
-            'sheet2': data[1]
+            'furniture': data[0],
+            'playlist': data[1]
         }
     }
 
     return data;
 }
 
+function convertImageLinks() {
+    for (var i = 0; i < data.playlist.length; i++) {
+        if (data.playlist[i].artistimage) {
+            data.playlist[i].artistimage = convertToGridUrl(data.playlist[i].artistimage, 1000);
+        }
+        if (data.playlist[i].pickedbyimage) {
+            data.playlist[i].pickedbyimage = convertToGridUrl(data.playlist[i].pickedbyimage, 140);
+        }
+    }
+
+    return data;
+}
+
+function convertToGridUrl(url, size) {
+    var crop = url.split('?crop=')[1];
+        
+        url = url.replace('gutools.co.uk', 'guim.co.uk');
+        url = url.replace('http://', 'https://');
+        url = url.replace('images/', '');
+        url = url.split('?')[0];
+
+    return url + '/' + crop + '/' + size + '.jpg';
+}
+
 module.exports = function getData() {
     var isDone = false;
 
+    console.log('hey');
+
     fetchData(function(result) {
         data = result;
-        data = sortResults(data);
+        data = sortResults();
+        data = setFurniture();
+        data = convertImageLinks();
+
+        console.log(data);
 
         isDone = true;
     });
